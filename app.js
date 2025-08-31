b8cc2403fbd4f3876c3e6fc030425e907910f9ec
tanu-rath <rathoretanu567@gmail.com>
2025-08-31 18:13:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
 
+// Disable Express 'X-Powered-By' header for security
+app.disable("x-powered-by");
+
+const PORT = 3000;
 app.get("/", (req, res) => {
   res.send("Hello World from DevSecOps Pipeline ");
 });
 
 app.listen(PORT, () => {
-  console.log(`App running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
